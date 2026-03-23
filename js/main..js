55757a577309613822f898cfc235743b253fdf78
sahzebalam-blip <sahzeb.alam@gmail.com>
2026-03-23 14:53:09 +0530

    Update main..js

--- js/main..js
+++ js/main..js
@@ -104,3 +104,16 @@ if (!document.body.classList.contains("venx-page")) {
     });
   });
 }
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = (e.clientX - rect.left - rect.width / 2) / 20;
+    const y = (e.clientY - rect.top - rect.height / 2) / 20;
+
+    card.style.transform = `rotateX(${ -y }deg) rotateY(${ x }deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0)";
+  });
+});
